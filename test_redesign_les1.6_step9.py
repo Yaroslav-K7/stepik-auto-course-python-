bf5ddf19a065afd683ff4c5799aa5af4058ef920
Yaroslav <yaroslav.kuznetsov1994@gmail.com>
2020-03-16 21:26:06 +0200

    fix element_located

--- test_redesign_les1.6_step9.py
+++ test_redesign_les1.6_step9.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 import unittest
 from selenium.webdriver.common.by import By
-import time
 
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 link1 = "http://suninjuly.github.io/registration1.html"
@@ -23,12 +23,12 @@ def r_browser(self, link):
         "test1@test.com")
     button = browser.find_element(By.CSS_SELECTOR, "button.btn").click()
 
-    time.sleep(1)
+
     welcome_text_elt = browser.find_element_by_tag_name("h1")
     welcome_text = welcome_text_elt.text
 
-    # welcome_text = WebDriverWait(browser, 3).until(
-    #     EC.presence_of_element_located(By.TAG_NAME, "h1")).text
+    welcome_text = WebDriverWait(browser, 7).until(
+        EC.presence_of_element_located((By.TAG_NAME, "h1"))).text
     self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
 
     browser.quit()
